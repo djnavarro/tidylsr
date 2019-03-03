6d939277f2db3fdb52dde04e67a9df9b851d459e
Danielle Navarro <d.navarro@unsw.edu.au>
2019-03-03 18:41:25 +1100

    first pass at workspace function

---
+++ R/workspace.R
@@ -0,0 +1,42 @@
+#' @importFrom magrittr %>%
+#' @importFrom tibble tibble
+#' @importFrom purrr map_dbl
+NULL
+
+#' Prints the contents of the global environment
+#'
+#' @return Something
+#' @examples
+#' workspace()
+#' @export
+workspace <- function() {
+
+  # the callin environment
+  call_env <- parent.frame()
+
+  # takes a character vector of variable names
+  # and returns the (first) class of each variable
+  get_classes <- function(x) {
+    class(eval(as.symbol(x)))[1]
+  }
+
+  # takes a character vector of variable names
+  # and returns the length of each variable
+  get_lengths <- function(x) {
+    length(eval(as.symbol(x)))
+  }
+
+
+  eval_there <- function(fn) {
+    eval(
+      expr = sapply(objects(envir = call_env), fn),
+      envir = call_env
+    )
+  }
+
+  wspace <- tibble(name = objects(envir = call_env))
+  wspace$class <- eval_there(get_classes)
+  wspace$length <- eval_there(get_lengths)
+
+  return(wspace)
+}
