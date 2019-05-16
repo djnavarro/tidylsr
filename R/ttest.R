4cbadca368250d965da6ab01101b9684cb4eb99a
Danielle Navarro <d.navarro@unsw.edu.au>
2019-05-16 15:11:05 +1000

    allow model formulas

--- R/ttest.R
+++ R/ttest.R
@@ -1,9 +1,10 @@
 
-#' Independent samples t-test
+#' Twor samples t-test
 #'
 #' @param data a data frame or tibble
+#' @param formula the model formula (i.e., outcome ~ group)
 #' @param outcome the outcome variable (quoted)
-#' @param group the grouping variable (quote)
+#' @param group the grouping variable (quoted)
 #' @importFrom rlang enquo
 #' @importFrom rlang expr
 #' @importFrom dplyr group_by
@@ -14,22 +15,26 @@
 #' @importFrom stats as.formula
 #' @importFrom stats sd
 #' @export
-ttest_twosample <- function(data, outcome, group) {
+ttest_twosample <- function(data, formula = NULL, outcome = NULL, group = NULL) {
 
-  # construct formula
-  lhs <- rlang::enexpr(outcome)
-  rhs <- rlang::enexpr(group)
-  fml <- as.formula(call("~", lhs, rhs))
+  # construct formula if needed
+  if(!is.null(formula)) {
+    outcome <- rlang::enexpr(outcome)
+    group <- rlang::enexpr(group)
+    formula <- as.formula(call("~", outcome, group))
+  }
 
   # run the t-test
-  ttest <- stats::t.test(fml, data)
+  ttest <- stats::t.test(formula, data)
 
   # descriptives
   desc <- data %>%
-    dplyr::group_by(!!rhs) %>%
-    dplyr::summarise(mean(!!lhs), sd(!!lhs)) %>%
+    dplyr::group_by(!!group) %>%
+    dplyr::summarise(mean(!!outcome), sd(!!outcome)) %>%
     dplyr::ungroup()
 
-  return(list(desc,ttest))
+  result <- list(desc, ttest)
+
+  return(result)
 }
 
