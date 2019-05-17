607484978bca5d0dbcf1b3e376500ac7e1f68a77
Danielle Navarro <d.navarro@unsw.edu.au>
2019-05-17 16:55:46 +1000

    add testthat for one sided t-test

--- tests/testthat/test-ttest.R
+++ tests/testthat/test-ttest.R
@@ -1,6 +1,6 @@
 tbl <- tibble(
-  out = rnorm(n = 20),
-  grp = sample(c("A","B"), size = 20, replace = TRUE)
+  out = rnorm(n = 40) + rep(c(0,1), 20),
+  grp = rep(c("grp A", "grp B"), 20) # names with spaces
 )
 
 
@@ -12,5 +12,21 @@ test_that("all three versions give the same answer", {
 
   expect_equal(tst1, tst2)
   expect_equal(tst2, tst3)
+
+})
+
+test_that("one and two-sided p-values add appropriately", {
+
+  tst1 <- ttest_twosample(tbl, out ~ grp, alternative = "grp A" < "grp B")
+  tst2 <- ttest_twosample(tbl, out ~ grp, alternative = "grp B" < "grp A")
+  tst3 <- ttest_twosample(tbl, out ~ grp)
+
+
+  p1 <- tst1$p
+  p2 <- tst2$p
+  p3 <- tst3$p
+
+  expect_equal(p1 + p2, 1)
+  expect_equal(min(p1,p2), p3/2)
 
 })
