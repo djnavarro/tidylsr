d5fa59e045bcab4f716ffd126587fea12b07f203
Danielle Navarro <d.navarro@unsw.edu.au>
2019-06-01 14:33:13 +1000

    Added initial documentation for the sampling frames data set

---
+++ R/samplingframes-data.R
@@ -0,0 +1,10 @@
+#' Data set on human inductive reasoning
+#'
+#' Experiment 2 from Hayes et al (in press).
+#'
+#' @docType data
+#' @usage data(samplingframes)
+#' @format a tibble
+#' @references Hayes et al
+#' @source https://psyarxiv.com/2m83v/
+"samplingframes"
